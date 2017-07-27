682c66e8d2e146787ed0f6d5c0ec4fa5aff80765
siromanto <dmytrenko.serhii@gmail.com>
2017-07-27 16:58:04 +0300

    created new cards

--- js/controllers/listCtrl.js
+++ js/controllers/listCtrl.js
@@ -1,6 +1,16 @@
-angular.module('app').controller('listCtrl', function (listFactory) {
+angular.module('app').controller('listCtrl', function (listFactory, cardFactory) {
 
     this.removeList = function (list) {
         listFactory.removeList(list);
     };
+
+    this.getCards = function (list) {
+        return cardFactory.getCards(list);
+    };
+
+    this.createCard = function (list) {
+        console.log('this.cardDescription', this.cardDescription);
+        cardFactory.createCard(list, this.cardDescription);
+        this.cardDescription = '';
+    }
 });
